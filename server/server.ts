ee6b565be6504ab3d01ebbe24451d2ac11cd6786
Ishaan-Wadhwa-tech <ishaanwadhwa.work@gmail.com>
2026-01-30 12:44:51 +0530

    sameSite setting changed

--- server/server.ts
+++ server/server.ts
@@ -26,7 +26,7 @@ app.use(session({
         maxAge:1000*60*60*24*7,
         httpOnly:true,
         secure:process.env.NODE_ENV === 'production',
-        sameSite:'none',
+        sameSite:process.env.NODE_ENV === 'production'?'none':'lax',
         path:'/'
     },
     store:MongoStore.create({
