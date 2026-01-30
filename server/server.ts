8cbdce7dc7128c21ab2cfec19a6fcb6f4b6639f0
Ishaan-Wadhwa-tech <ishaanwadhwa.work@gmail.com>
2026-01-30 12:04:23 +0530

    changed the server.ts

--- server/server.ts
+++ server/server.ts
@@ -32,7 +32,7 @@ app.use(session({
 }))
 app.use(express.json())
 app.use(cors({
-    origin:['http://localhost:5173','http://localhost:3000',"https://thumblify-drab.vercel.app/"],
+    origin:['http://localhost:5173','http://localhost:3000',"https://thumblify-drab.vercel.app"],
     credentials:true
 }))
 
